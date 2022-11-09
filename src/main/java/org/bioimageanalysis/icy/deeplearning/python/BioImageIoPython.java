135df989088e3c3aebfe98d4f19a98e8b75058c3
Cgarcia <Cgarcia@192.168.0.22>
2022-11-09 23:00:51 +0100

    remove unused import

--- src/main/java/org/bioimageanalysis/icy/deeplearning/python/BioImageIoPython.java
+++ src/main/java/org/bioimageanalysis/icy/deeplearning/python/BioImageIoPython.java
@@ -2,7 +2,6 @@ package org.bioimageanalysis.icy.deeplearning.python;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
